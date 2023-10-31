c4a16124a8877a568642670fb24bdf4e65c96e4b
Denys Synko <denissinko2004@gmail.com>
2023-10-31 14:14:23 +0200

    commit

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import { createWebHashHistory, createRouter } from "vue-router";
+import store from './store.js'
+
+import App from "./components/App.vue";
+import Students from "./components/Students.vue";
+import StudentInfo from "./components/StudentInfo.vue";
+
+const routes = [
+  { path: "/", component: Students },
+  { path: "/student-info/:id", component: StudentInfo, props: true },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+createApp(App).use(router).use(store).mount("#app");
+
